55ec2354fd6c969444bdd9faf4f3806b07ea6b48
Fra3z49 <112791937+Fra3z49@users.noreply.github.com>
2024-04-06 18:51:21 -0500

    Buttons can be clisked to add the shown number to an array stored as selected_number.

--- index.js
+++ index.js
@@ -51,14 +51,89 @@ if(operator === '+') {
  //Populates the display based upon the number selected.
 
  function populate() {
+ 
+    let selected_number = [];
+
+//Adds a number to the number variable when the number is selected.
+const btn_one = document.querySelector('#one');
+    btn_one.addEventListener('click', () => {
+        selected_number.push(1)
+        console.log (selected_number);
+    } 
+)
+
+
+const btn_two = document.querySelector('#two');
+    btn_two.addEventListener('click', () => {
+        selected_number.push(2)
+        console.log (selected_number);
+    } 
+)
+
+const btn_three = document.querySelector('#three');
+    btn_three.addEventListener('click', () => {
+        selected_number.push(3)
+        console.log (selected_number);
+} 
+)
+
+const btn_four = document.querySelector('#four');
+    btn_four.addEventListener('click', () => {
+        selected_number.push(4)
+        console.log (selected_number);
+} 
+)
+
+const btn_five = document.querySelector('#five');
+    btn_five.addEventListener('click', () => {
+        selected_number.push(5)
+        console.log (selected_number);
+} 
+)
+
+const btn_six = document.querySelector('#six');
+    btn_six.addEventListener('click', () => {
+        selected_number.push(6)
+        console.log (selected_number);
+} 
+)
+
+const btn_seven = document.querySelector('#seven');
+    btn_seven.addEventListener('click', () => {
+        selected_number.push(7)
+        console.log (selected_number);
+} 
+)
+
+const btn_eight = document.querySelector('#eight');
+    btn_eight.addEventListener('click', () => {
+        selected_number.push(8)
+        console.log (selected_number);
+} 
+)
+
+const btn_nine = document.querySelector('#nine');
+    btn_nine.addEventListener('click', () => {
+        selected_number.push(9)
+        console.log (selected_number);
+} 
+)
+
+const btn_zero = document.querySelector('#zero');
+    btn_zero.addEventListener('click', () => {
+        selected_number.push(0)
+        console.log (selected_number);
+} 
+)
+
+
+//Selected the ID=display elements and adds a "p" element. Adds the selected numbers to the display.
     const display = document.querySelector('#display');
     const display_element = document.createElement('H2');
-        display_element.textContent = 'One Two Three Four'; //Creates an element for the display.
+        display_element.textContent = selected_number; //Creates an element for the display.
     display.appendChild(display_element);
  }
 
 //Tests the operate function and logs them in the console. Shows a multiply function and a error in the console.
 
-console.log (operate(13, '*', 18));
-console.log (operate(6, "a", 10));
-populate();
+console.log (populate());
