07f6b37d406dd66db758534ac9ff195ee1e48734
Fra3z49 <112791937+Fra3z49@users.noreply.github.com>
2024-04-06 17:05:26 -0500

    Basic add, subtract, multiply, and devide functions.

---
+++ index.js
@@ -0,0 +1,28 @@
+
+
+// Math functions (add, subtract, multiply, divide)
+
+function add (num1 ,num2) {
+    let result = num1 + num2;
+    return result;
+}
+
+function subtract (num1, num2) {
+    let result = num1 - num2;
+    return result;
+}
+
+function multiply (num1, num2) {
+    let result = num1 * num2;
+    return result;
+}
+
+function divide (num1, num2) {
+    let result = num1/num2;
+    return result;
+}
+
+
+//Test the function results and log them in console.
+
+console.log (add (1,1), subtract(1,1), multiply(1,2), divide(6,2));
