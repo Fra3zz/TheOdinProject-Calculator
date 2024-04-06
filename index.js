10f1a20424078a444e32873888e5e64b0e51c81c
Fra3z49 <112791937+Fra3z49@users.noreply.github.com>
2024-04-06 17:13:09 -0500

    Creation to three variables. - num1 -operator -num2

--- index.js
+++ index.js
@@ -23,6 +23,12 @@ function divide (num1, num2) {
 }
 
 
+
+// creates three variables, num1, operator, and num2.
+let num1 = 1;
+let operator = 'opeartor_input'; //Will be further defined later.
+letnum2 = 2;
+
 //Test the function results and log them in console.
 
 console.log (add (1,1), subtract(1,1), multiply(1,2), divide(6,2));
