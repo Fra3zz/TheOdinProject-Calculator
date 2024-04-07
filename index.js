5095f1b162514b17d20e85dbad4ced27c3e74f68
Fra3z49 <112791937+Fra3z49@users.noreply.github.com>
2024-04-06 19:10:46 -0500

    - Adds the number to an array. - On a number push, the array is added to the display element - Eacch number shows the array without the commas by goining the numbers.

--- index.js
+++ index.js
@@ -52,86 +52,108 @@ if(operator === '+') {
 
  function populate() {
  
-    let selected_number = [];
+    let selected_numbers = [];
+
+
+//Selected the ID=display elements and adds a "p" element. Adds the selected numbers to the display.
+const display = document.querySelector('#display');
+const display_element = document.createElement('H2');
+
+
 
 //Adds a number to the number variable when the number is selected.
 const btn_one = document.querySelector('#one');
     btn_one.addEventListener('click', () => {
-        selected_number.push(1)
-        console.log (selected_number);
+        selected_numbers.push(1)
+        console.log (selected_numbers);
+        display_element.textContent = selected_numbers.join("");
+        display.appendChild(display_element);
     } 
 )
 
-
 const btn_two = document.querySelector('#two');
     btn_two.addEventListener('click', () => {
-        selected_number.push(2)
-        console.log (selected_number);
+        selected_numbers.push(2)
+        console.log (selected_numbers);
+        display_element.textContent = selected_numbers.join("");
+        display.appendChild(display_element);
     } 
 )
 
 const btn_three = document.querySelector('#three');
     btn_three.addEventListener('click', () => {
-        selected_number.push(3)
-        console.log (selected_number);
-} 
+        selected_numbers.push(3)
+        console.log (selected_numbers);
+        display_element.textContent = selected_numbers.join("");
+        display.appendChild(display_element);
+    } 
 )
 
 const btn_four = document.querySelector('#four');
     btn_four.addEventListener('click', () => {
-        selected_number.push(4)
-        console.log (selected_number);
-} 
+        selected_numbers.push(4)
+        console.log (selected_numbers);
+        display_element.textContent = selected_numbers.join("");
+        display.appendChild(display_element);
+    } 
 )
 
 const btn_five = document.querySelector('#five');
     btn_five.addEventListener('click', () => {
-        selected_number.push(5)
-        console.log (selected_number);
-} 
+        selected_numbers.push(5)
+        console.log (selected_numbers);
+        display_element.textContent = selected_numbers.join("");
+        display.appendChild(display_element);
+    } 
 )
 
 const btn_six = document.querySelector('#six');
     btn_six.addEventListener('click', () => {
-        selected_number.push(6)
-        console.log (selected_number);
-} 
+        selected_numbers.push(6)
+        console.log (selected_numbers);
+        display_element.textContent = selected_numbers.join("");
+        display.appendChild(display_element);
+    } 
 )
 
 const btn_seven = document.querySelector('#seven');
     btn_seven.addEventListener('click', () => {
-        selected_number.push(7)
-        console.log (selected_number);
-} 
+        selected_numbers.push(7)
+        console.log (selected_numbers);
+        display_element.textContent = selected_numbers.join("");
+        display.appendChild(display_element);
+    } 
 )
 
 const btn_eight = document.querySelector('#eight');
     btn_eight.addEventListener('click', () => {
-        selected_number.push(8)
-        console.log (selected_number);
-} 
+        selected_numbers.push(8)
+        console.log (selected_numbers);
+        display_element.textContent = selected_numbers.join("");
+        display.appendChild(display_element);
+    } 
 )
 
 const btn_nine = document.querySelector('#nine');
     btn_nine.addEventListener('click', () => {
-        selected_number.push(9)
-        console.log (selected_number);
-} 
+        selected_numbers.push(9)
+        console.log (selected_numbers);
+        display_element.textContent = selected_numbers.join("");
+        display.appendChild(display_element);
+
+    } 
 )
 
 const btn_zero = document.querySelector('#zero');
     btn_zero.addEventListener('click', () => {
-        selected_number.push(0)
-        console.log (selected_number);
-} 
+        selected_numbers.push(0)
+        console.log (selected_numbers);
+        display_element.textContent = selected_numbers.join("");
+        display.appendChild(display_element);
+    } 
 )
 
 
-//Selected the ID=display elements and adds a "p" element. Adds the selected numbers to the display.
-    const display = document.querySelector('#display');
-    const display_element = document.createElement('H2');
-        display_element.textContent = selected_number; //Creates an element for the display.
-    display.appendChild(display_element);
  }
 
 //Tests the operate function and logs them in the console. Shows a multiply function and a error in the console.
